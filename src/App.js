0039eb9af9a3029dc7ce9e7fa25b2f9c2193f257
aadi0608 <adarshtiwari2at@gmail.com>
2024-02-22 16:11:58 +0530

    tic-tac-toe

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TicTacToe from './Componants/TIcTacToe/TicTacToe';
+
+function App() {
+  return (
+    <div>
+      <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
